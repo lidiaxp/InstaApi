bb87cb87b601f1fa3b2ebfc012b2e904a4a62950
Lidia Gianne <lidiagianne@gmail.com>
2017-09-26 23:24:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,255 +1,92 @@
-var express = require('express'),
-	bodyParser = require('body-parser'),
-	//mongodb = require('mongodb'),
-        mongoose = require('mongoose');//,
-	//multiparty = require('connect-multiparty'),
-	//fs = require('fs'),
-	//objectId = require('mongodb').ObjectId;
+// https://devcenter.heroku.com/articles/mongolab
+// http://todomvc.com/examples/angularjs/#/
+var express  = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
 
-	var app = express();
+    // Mongoose Schema definition
+    Schema = new mongoose.Schema({
+      id       : String, 
+      title    : String,
+      completed: Boolean
+    }),
 
-	/*app.use(bodyParser.urlencoded({extended:true}));
-	app.use(bodyParser.json());
-	app.use(multiparty());
+    Todo = mongoose.model('Todo', Schema);
 
-	app.use(function(req, res, next){
-		res.setHeader("Access-Control-allow-Origin", "*");
-		res.setHeader("Access-Control-allow-Methods", "GET, POST, PUT, DELETE");
-		res.setHeader("Access-Control-allow-Headers", "Content-type");
-		res.setHeader("Access-Control-allow-Credentials", true);
+/*
+ * I’m sharing my credential here.
+ * Feel free to use it while you’re learning.
+ * After that, create and use your own credential.
+ * Thanks.
+ *
+ * MONGOLAB_URI=mongodb://example:example@ds053312.mongolab.com:53312/todolist
+ * 'mongodb://example:example@ds053312.mongolab.com:53312/todolist'
+ */
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
 
-		next();
-	});*/
-
-	Schema = new mongoose.Schema({
-      		id       : String, 
-      		title    : String,
-      		completed: Boolean
-    	}),
-
-    	Todo = mongoose.model('Todo', Schema);
-
-	//var port = 8080;
-
-	//app.listen(port);
-
-	/*var db = new mongodb.Db(
-		'instagram',
-		new mongodb.Server('localhost', 27017, {}),
-		{}
-	);*/
-
-	mongoose.connect(process.env.MONGOLAB_URI, function (error) {
-	    if (error) console.error(error);
-    	    else console.log('mongo connected');
- 	});
-
-	
+express()
   // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
-  	app.use(bodyParser.json()) // support json encoded bodies
-  	app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+  .use(bodyParser.json()) // support json encoded bodies
+  .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-	//console.log('Servidor HTTP esta escutando na porta ' + port);
+  .get('/api', function (req, res) {
+    res.json(200, {msg: 'OK' });
+  })
 
-	app.get('/api', function(req, res){
-	
-		res.json(200, {msg: 'OK' });
-	});
+  .get('/api/todos', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-find
+    Todo.find( function ( err, todos ){
+      res.json(200, todos);
+    });
+  })
 
-	//POST
-	app.post('/api', function(req, res){
-		//res.setHeader("Access-Control-allow-Origin", "*"); //ou coloca a url no lugar do * se quiser limitar as chamadas
-	
-		var date = new Date();
-		time_stamp = date.getTime();
+  .post('/api/todos', function (req, res) {
+    var todo = new Todo( req.body );
+    todo.id = todo._id;
+    // http://mongoosejs.com/docs/api.html#model_Model-save
+    todo.save(function (err) {
+      res.json(200, todo);
+    });
+  })
 
-		var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+  .del('/api/todos', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-remove
+    Todo.remove({ completed: true }, function ( err ) {
+      res.json(200, {msg: 'OK'});
+    });
+  })
 
-		var path_origem = req.files.arquivo.path;
-		var path_destino = './uploads/' + url_imagem;
+  .get('/api/todos/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    Todo.findById( req.params.id, function ( err, todo ) {
+      res.json(200, todo);
+    });
+  })
 
-		fs.rename(path_origem, path_destino, function(err){
-			if(err){
-				res.status(500).json({error: err});
-				return;
-			}
+  .put('/api/todos/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    Todo.findById( req.params.id, function ( err, todo ) {
+      todo.title = req.body.title;
+      todo.completed = req.body.completed;
+      // http://mongoosejs.com/docs/api.html#model_Model-save
+      todo.save( function ( err, todo ){
+        res.json(200, todo);
+      });
+    });
+  })
 
-			var dados = {
-				url_imagem: url_imagem,
-				titulo: req.body.titulo
-			}
+  .del('/api/todos/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    Todo.findById( req.params.id, function ( err, todo ) {
+      // http://mongoosejs.com/docs/api.html#model_Model.remove
+      todo.remove( function ( err, todo ){
+        res.json(200, {msg: 'OK'});
+      });
+    });
+  })
 
-			db.open(function(err, mongoClient){
-				mongoClient.collection('postagens', function(err, collection){
-					collection.insert(dados, function(err, records){
-						if(err){
-							res.json({'status' : 'Erro'});
-						}else{
-							res.json({'status' : 'Inclusão realizada com sucesso'});
-						}
-						mongoClient.close();
-					});
-				});
-			});
-		});
-	});
-
-	//GET all
-	/*app.get('/api', function(req, res){
-		res.send({msg: 'oi'});
-		res.json(200, {msg: 'OK' });
-		//res.setHeader("Access-Control-allow-Origin", "*"); //ou coloca a url no lugar do * se quiser limitar as chamadas
-
-		db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.find().toArray(function(err, results){
-					if(err){
-						res.json(err);
-					}else{
-						res.json(results);
-					}
-					mongoClient.close();
-				});
-			});
-		});
-	});*/
-
-	//GET por id
-	app.get('/api/:id', function(req, res){
-		db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.find(objectId(req.params.id)).toArray(function(err, results){
-					if(err){
-						res.json(err);
-					}else{
-						/*if(results === null){
-							res.status(res.status(500).json(results));
-						}else{*/
-							res.json(results);
-						//}
-					}
-					mongoClient.close();
-				});
-			});
-		});
-	});
-
-	//GET por whatever
-	/*app.get('/api/:titulo', function(req, res){
-		db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.find({titulo : req.params.titulo}).toArray(function(err, results){
-					if(err){
-						res.json(err);
-					}else{
-						res.json(results);
-					}
-					mongoClient.close();
-				});
-			});
-		});
-	});*/
-
-	app.get('/imagens/:imagem', function(req, res){
-		var img = req.params.imagem;
-
-		fs.readFile('./uploads/' + img, function(err, conteudo){
-			if(err){
-				res.status(400).json(err);
-				return;
-			}
-
-			res.writeHead(200, {'Content-type':'image/png', 'Content-type':'image/jpg'});
-			res.end(conteudo);
-		})
-	});
-
-	//PUT por id
-	app.put('/api/:id', function(req, res){
-		//res.send(req.body.comentario);
-
-		db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.update(
-					{ _id : objectId(req.params.id) },
-					{ $push : {
-								comentarios : {
-									id_comentario : new objectId(),
-									comentario : req.body.comentario
-								}
-							  }
-					},
-					{ },
-					function(err, records){
-						if(err){
-							res.json(err);
-						}else{
-							res.json(records);
-						}
-						mongoClient.close();
-					}
-				);
-			});
-		});
-
-		/*db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.update(
-					{ _id : objectId(req.params.id) },
-					{ $set : {titulo : req.body.titulo}},
-					{ },
-					function(err, records){
-						if(err){
-							res.json(err);
-						}else{
-							res.json(records);
-						}
-						mongoClient.close();
-					}
-				);
-			});
-		});*/
-	});
-
-	//DELETE por id
-	app.delete('/api/:id', function(req, res){
-		db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.update(
-					{ },
-					{ $pull : {
-								comentarios: {id_comentario: objectId(req.params.id)}
-							  } 
-					},
-					{multi: true},
-					function(err, records){
-						if(err){
-							res.json(err);
-						}else{
-							res.json(records);
-						}
-						mongoClient.close();
-					}
-				);
-			});
-		});
-
-		/*db.open(function(err, mongoClient){
-			mongoClient.collection('postagens', function(err, collection){
-				collection.remove(
-					{ _id: objectId(req.params.id) },
-					function(err, records){
-						if(err){
-							res.json(err);
-						}else{
-							res.json(records);
-						}
-						mongoClient.close();
-					}
-				);
-			});
-		});*/
-	});
-
-	app.use(express.static(__dirname + '/'));
-        app.listen(process.env.PORT || 5000);
+  .use(express.static(__dirname + '/'))
+  .listen(process.env.PORT || 5000);
